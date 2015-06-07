592e67d7219d38d779ca224e59ee815fdc48920b
ayette <mlav.ph@gmail.com>
2015-06-08 07:28:01 +0800

    1st

---
+++ MultipleDuckDNSUpdater/Settings.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace MultipleDuckDNSUpdater
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.lstSubDomain = new System.Windows.Forms.ListBox();
+            this.lblSubDomain = new System.Windows.Forms.Label();
+            this.lblToken = new System.Windows.Forms.Label();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "DuckDNS Updater";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+            // 
+            // lstSubDomain
+            // 
+            this.lstSubDomain.FormattingEnabled = true;
+            this.lstSubDomain.Location = new System.Drawing.Point(12, 86);
+            this.lstSubDomain.Name = "lstSubDomain";
+            this.lstSubDomain.Size = new System.Drawing.Size(347, 95);
+            this.lstSubDomain.TabIndex = 0;
+            // 
+            // lblSubDomain
+            // 
+            this.lblSubDomain.AutoSize = true;
+            this.lblSubDomain.Location = new System.Drawing.Point(13, 67);
+            this.lblSubDomain.Name = "lblSubDomain";
+            this.lblSubDomain.Size = new System.Drawing.Size(65, 13);
+            this.lblSubDomain.TabIndex = 1;
+            this.lblSubDomain.Text = "Sub Domain";
+            // 
+            // lblToken
+            // 
+            this.lblToken.AutoSize = true;
+            this.lblToken.Location = new System.Drawing.Point(12, 12);
+            this.lblToken.Name = "lblToken";
+            this.lblToken.Size = new System.Drawing.Size(38, 13);
+            this.lblToken.TabIndex = 2;
+            this.lblToken.Text = "Token";
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(12, 28);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.ReadOnly = true;
+            this.txtToken.Size = new System.Drawing.Size(347, 20);
+            this.txtToken.TabIndex = 3;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(284, 191);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 60000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 225);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.lblToken);
+            this.Controls.Add(this.lblSubDomain);
+            this.Controls.Add(this.lstSubDomain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Settings";
+            this.Text = "DuckDNS.org Setting";
+            this.Load += new System.EventHandler(this.Settings_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ListBox lstSubDomain;
+        private System.Windows.Forms.Label lblSubDomain;
+        private System.Windows.Forms.Label lblToken;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ MultipleDuckDNSUpdater/Settings.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultipleDuckDNSUpdater
+{
+    public partial class Settings : Form
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            base.OnDeactivate(e);
+            this.Hide();            
+            timer.Enabled = true;
+            timer_Tick(this, e);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            var subdomain = new List<string>(ConfigurationManager.AppSettings["subdomain"].Split(new char[] { ';' }));
+
+            foreach (string s in subdomain)
+            {
+                UpdateIP(s);
+            }
+        }
+
+        public async Task UpdateIP(string subdomain)
+        {
+            try
+            {
+
+                HttpClient client = new HttpClient();
+
+
+                Task<string> getStringTask = client.GetStringAsync(string.Format("https://duckdns.org/update/{0}/{1}", subdomain, ConfigurationManager.AppSettings["token"]));
+
+                string urlContents = await getStringTask;
+                Console.WriteLine(string.Format("Subdomain: {0}, Status: {1}",subdomain, urlContents));                
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void Settings_Load(object sender, EventArgs e)
+        {
+            txtToken.Text = ConfigurationManager.AppSettings["token"].ToString();
+
+            var subdomain = new List<string>(ConfigurationManager.AppSettings["subdomain"].Split(new char[] { ';' }));
+
+            lstSubDomain.Items.Clear();
+            foreach (string s in subdomain)
+            {
+                lstSubDomain.Items.Add(s);
+            }
+        }
+
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.Activate();
+        }
+
+
+    }
+}
